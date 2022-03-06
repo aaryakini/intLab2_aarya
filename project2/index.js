21807892cc2c3cf1e75b9b4059c3170410bb4b70
aaryakini <kinia313@newschool.edu>
2022-03-06 12:43:43 -0500

    adding links

--- project2/index.js
+++ project2/index.js
@@ -1,21 +1,11 @@
-function liddell(){
-	var x = document.getElementById("liddell");
+function showHide(name){
+	let x = document.getElementById(name);
 	if (x.style.display === "none") {
 		x.style.display = "block";
 	} else {
 		x.style.display = "none";
 	}
 }
-
-function montauban(){
-	var x = document.getElementById("montauban");
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	}
-}
-
 
 /***************************************************************************************
 *    Title: JavaScript - How to make multiple divs draggable and movable across the website
@@ -25,40 +15,42 @@ function montauban(){
 ***************************************************************************************/
 
 var draggableElements = document.getElementsByClassName("tooltipframe");
+console.log(draggableElements.length); //checking that array exists and is populated
 
 for(var i = 0; i < draggableElements.length; i++){
     dragElement(draggableElements[i]);
 }
 
-function dragElement(elmnt) {
-	console.log("works!");
+function dragElement(element) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
+    element.onmousedown = dragMouseDown;
+
     function dragMouseDown(e) {
         e = e || window.event;
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
         document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
-        return false;
     }
 
     function elementDrag(e) {
         e = e || window.event;
-        pos1 = pos3 - parseInt(e.clientX);
-        pos2 = pos4 - parseInt(e.clientY);
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        console.log(elmnt.offsetTop)
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
     }
 
     function closeDragElement() {
+        // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
     }
